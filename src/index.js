b9336db466ab94e516832c0c6197ef5dd67a6b2d
Yaqoob <sualehfarooq65@gmail.com>
2022-11-23 01:38:38 -0800

    commit

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ root.render(
   <React.StrictMode>
     <>
       <div className="noti">
-        <p>Turo has launched in New York! Tap to explore cars.</p>
+        <p> <a href="https://turo.com/us/en/search?country=US&defaultZoomLevel=11&delivery=false&endDate=11%2F28%2F2022&endTime=10%3A00&isMapSearch=false&itemsPerPage=200&latitude=40.7127753&location=New%20York%2C%20NY%2C%20USA&locationType=CITY&longitude=-74.0059728&pickupType=ALL&region=NY&sortType=RELEVANCE&startDate=11%2F25%2F2022&startTime=10%3A00&useDefaultMaximumDistance=true">Turo has launched in New York! Tap to explore cars.</a> </p>
       </div>
       <CollapsibleExample />
       <UncontrolledExample />
